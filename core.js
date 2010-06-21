ca4458aa9f656982faf399c57e09fe5bc113ab4e
Leland Tseng <leland@leland-desktop.(none)>
2010-06-21 11:17:10 +0800

    完成Display.draw绘制方法雏形

--- core.js
+++ core.js
@@ -116,44 +116,35 @@
     game.Display = function(map){
     
         // 地图
-        var this_map;
+        var this_map, this_map_data;
         this_map = map;
+        this_map_data = map.getData();
         
         
-        //参照点
-        this.refer_point = {
+        //参照点 是像素表示，不是单元格
+        this.info = {
             x: 0,
-            y: 0
+            y: 0,
+            width: 100,
+            height: 100
         };
-        
-        // 屏幕宽度  
-        this.width = 50;
-        
-        // 屏幕高度
-        this.height = 50;
         
         // 绘制屏幕
         this.draw = function(){
-			
-			// 
-			
-			
-			
-			
+        
+            var drawinfo = drawInfo(this.info);
+            
             var cxt = document.getElementById("game").getContext('2d');
-            var mapdata = this_map.getData();
             var xyobj = mapdata[this.refer_point.y][this.refer_point.x]; // 做上角的对象。
-            var drawX,drawY; // 绘制原点
-            if(xyobj != null && !(xyobj instanceof game.Comp)){
-			  // this.refer_point.y - xyobj.x	
-			}	   
-		   
-            for (var j = 0; j < mapdata.length; j++) {
+            if (xyobj != null && !(xyobj instanceof game.Comp)) {
+                // this.refer_point.y - xyobj.x	
+            }
+            
+            for (var j = 0; j < drawinfo.mapH; j++) {
                 var hang = mapdata[j];
-                for (var i = 0; i < hang.length; i++) {
+                for (var i = 0; i < drawinfo.mapW; i++) {
                     if (hang[i] instanceof game.Comp) {
-                        
-                        cxt.drawImage(hang[i].img_current, i * 5, j * 5, hang[i].size * 5, hang[i].size * 5);
+                        cxt.drawImage(hang[i].img_current, i * this_map.getUnitSize() + info.offsetX, j * this_map.getUnitSize() + info.offsetY, hang[i].size * this_map.getUnitSize(), hang[i].size * this_map.getUnitSize());
                     }
                 }
                 
@@ -161,6 +152,26 @@
             setTimeout("draw()", 50)
         }
         
+        
+        //根据参照点{x,y} 计算出绘制原点 和 方格坐标 {drawX,drawY,compX，compY}
+        function drawInfo(info){
+        
+            var mapX_ = info.x / this_map.getUnitSize();
+            var mapY_ = info.y / this_map.getUnitSize();
+            var drawX_ = mapX * this_map.getUnitSize() - info.x;
+            var drawY_ = mapY * this_map.getUnitSize() - info.y;
+            var mapW_ = info.width / this_map.getUnitSize() + 1;
+            var mapH_ = info.height / this_map.getUnitSize() + 1;
+            
+            return {
+                mapX: mapX_,
+                mapY: mapY_,
+                offsetX: drawX_,
+                offsetY: drawY_,
+                mapW: mapW_,
+                mapH: mapH_
+            }
+        }
         
     }
     
