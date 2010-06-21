b23c89b48cafb6c3e693d5cfb5de609a5755f137
Leland Tseng <leland@leland-desktop.(none)>
2010-06-21 21:22:49 +0800

    实现了红警的地图阅览功能

--- core.js
+++ core.js
@@ -9,28 +9,29 @@
      height 地图高度
      unit_size 单元大小(像素)
      */
-    game.Map = function(width, height, unit_size){
-        var mapdata, this_width, this_height, this_unit_size;
+    game.Map = function(unit_size, canvas){
+        var canvas_, cxt;
+        canvas_ = canvas;
+        cxt = canvas.getContext('2d');
+        var mapdata, this_unit_size;
         mapdata = null
-        this_width = width
-        this_height = height
         this_unit_size = unit_size;
         // begin  生成地图数组
         mapdata = new Array();
-        for (var i = 0; i < this_height; i++) {
+        for (var i = 0; i < canvas.height / this_unit_size; i++) {
             mapdata[i] = new Array();
-            for (var j = 0; j < this_width; j++) {
+            for (var j = 0; j < canvas.width / this_unit_size; j++) {
                 mapdata[i][j] = null;
             }
-        }//end
+        }
         // 得到地图宽度
         this.getWidth = function(){
-            return this_width;
+            return canvas_.width;
         }
         
         // 得到地图高度
         this.getHeight = function(){
-            return this_height;
+            return canvas_.height;
         }
         
         // 得到地图单元大小
@@ -83,6 +84,18 @@
                 }
             
         }
+        
+        this.createMapImg = function(){
+            for (var j = 0; j < mapdata.length; j++) {
+                var hang = mapdata[j];
+                
+                for (var i = 0; i < hang.length; i++) {
+                    if (hang[i] instanceof game.Comp) {
+                        cxt.drawImage(hang[i].img_current, j * this_unit_size, i * this_unit_size, hang[i].size * this_unit_size, hang[i].size * this_unit_size);
+                    }
+                }
+            }
+        }
     }
     
     /* 游戏部件 */
@@ -97,81 +110,32 @@
         this.img_south_west = null;
         this.img_south_east = null;
         this.img_current = null;
-        
-        this.visible = false;
-        //  当前位置
-        // this.x = 10;
-        // this.y = 10;
-        
-        
-        // 移动
-        this.move = function(){
-        
-        }
     }
     
     /**
      * 屏幕
      */
-    game.Display = function(map){
+    game.Display = function(mapcanvas_, canvas_){
     
-        // 地图
-        var this_map, this_map_data;
-        this_map = map;
-        this_map_data = map.getData();
-        
+    
+        var canvas,mapcanvas;
+		canvas = canvas_;
+        var cxt = canvas.getContext('2d');
+        mapcanvas = mapcanvas_
         
         //参照点 是像素表示，不是单元格
         this.info = {
             x: 0,
-            y: 0,
-            width: 100,
-            height: 100
+            y: 0
         };
         
         // 绘制屏幕
         this.draw = function(){
-        
-            var drawinfo = drawInfo(this.info);
-            
-            var cxt = document.getElementById("game").getContext('2d');
-            var xyobj = mapdata[this.refer_point.y][this.refer_point.x]; // 做上角的对象。
-            if (xyobj != null && !(xyobj instanceof game.Comp)) {
-                // this.refer_point.y - xyobj.x	
-            }
-            
-            for (var j = 0; j < drawinfo.mapH; j++) {
-                var hang = mapdata[j];
-                for (var i = 0; i < drawinfo.mapW; i++) {
-                    if (hang[i] instanceof game.Comp) {
-                        cxt.drawImage(hang[i].img_current, i * this_map.getUnitSize() + info.offsetX, j * this_map.getUnitSize() + info.offsetY, hang[i].size * this_map.getUnitSize(), hang[i].size * this_map.getUnitSize());
-                    }
-                }
-                
-            }
-            setTimeout("draw()", 50)
+            //alert(mapcanvas.width);
+            cxt.drawImage(mapcanvas, this.info.x, this.info.y, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
         }
         
         
-        //根据参照点{x,y} 计算出绘制原点 和 方格坐标 {drawX,drawY,compX，compY}
-        function drawInfo(info){
-        
-            var mapX_ = info.x / this_map.getUnitSize();
-            var mapY_ = info.y / this_map.getUnitSize();
-            var drawX_ = mapX * this_map.getUnitSize() - info.x;
-            var drawY_ = mapY * this_map.getUnitSize() - info.y;
-            var mapW_ = info.width / this_map.getUnitSize() + 1;
-            var mapH_ = info.height / this_map.getUnitSize() + 1;
-            
-            return {
-                mapX: mapX_,
-                mapY: mapY_,
-                offsetX: drawX_,
-                offsetY: drawY_,
-                mapW: mapW_,
-                mapH: mapH_
-            }
-        }
         
     }
     
