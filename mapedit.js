03b4415ca5be24fa8cf2088f84c781b1135dc70b
Leland Tseng <leland@leland-desktop.(none)>
2010-07-09 17:22:12 +0800

    增加地图导出功能，有个小BUG。

--- mapedit.js
+++ mapedit.js
@@ -10,14 +10,47 @@
             var t = new ra.Tile();
             var img = new Image();
             img.src = 'resource/model01.png';
-			t.img = img;
+            t.img = img;
             t.size = 10;
-			//alert("x="+xx+"y="+yy+"t="+t.img.src);
-			
+            //alert("x="+xx+"y="+yy+"t="+t.img.src);
+            
             world.addTile(yy, xx, t);
             graphics.updateLayoutData();
             var bbb = graphics.createShowData()
             screen.putShowData(bbb);
+        }
+        
+        this.exportTileData = function(){
+            var data = world.getTileData();
+            var str = "{";
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < data[i].length; j++) {
+                    if (data[i][j] == null) 
+                        ;
+                    else 
+                        if (data[i][j] instanceof ra.Tile) {
+                            str = str + "p_" + i + "_" + j + ":{" +
+                            "img:'" +
+                            data[i][j].img.src +
+                            "',size:" +
+                            data[i][j].size+
+                            "},"
+                        }
+                        else {
+                            str = str + "p_" + i + "_" + j + ":{" +
+                            "x:" +
+                            data[i][j].x +
+                            ",y:" +
+                            data[i][j].y +
+                            "},"
+                        }
+                }
+             
+					 
+				
+            }
+            str = str + "}"
+			return str;
         }
         
     }
