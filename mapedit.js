ffa22ad9fc5794a5991699dc4c6bb9ad57d434f1
Leland Tseng <leland@leland-desktop.(none)>
2010-07-18 13:06:18 +0800

    添加了地图导出和导入功能

--- mapedit.js
+++ mapedit.js
@@ -10,14 +10,72 @@
             var t = new ra.Tile();
             var img = new Image();
             img.src = 'resource/model01.png';
-			t.img = img;
+            t.img = img;
             t.size = 10;
-			//alert("x="+xx+"y="+yy+"t="+t.img.src);
-			
+            //alert("x="+xx+"y="+yy+"t="+t.img.src);
+            
             world.addTile(yy, xx, t);
             graphics.updateLayoutData();
             var bbb = graphics.createShowData()
             screen.putShowData(bbb);
+        }
+        
+		/**
+		 * 生成地图数据
+		 */
+		this.importTileData = function(data){
+			var jsondata = eval('('+data+')');
+			for(var d in jsondata){
+				var d2 = d.toString();
+				ds = d2.split('_');
+				
+				if(jsondata[d].img){
+					var t = new ra.Tile();
+					var img = new Image();
+					img.src = jsondata[d].img;
+					t.img = img;
+					t.size = jsondata[d].size;
+					world.addTile(parseInt(ds[1]),parseInt(ds[2]),t);
+				}else{
+					
+				}
+			}
+		}
+		
+		/**
+		 * 生成地图数据
+		 */
+        this.exportTileData = function(){
+            var data = world.getTileData();
+            var str = "{";
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < data[i].length; j++) {
+                    if (data[i][j] == null) 
+                        ;
+                    else 
+                        if (data[i][j] instanceof ra.Tile) {
+                            str = str + "p_" + i + "_" + j + ":{" +
+                            "img:'" +
+                            data[i][j].img.src +
+                            "',size:" +
+                            data[i][j].size+
+                            "},"
+                        }
+                        else {
+                            str = str + "p_" + i + "_" + j + ":{" +
+                            "x:" +
+                            data[i][j].x +
+                            ",y:" +
+                            data[i][j].y +
+                            "},"
+                        }
+                }
+             
+					 
+				
+            }
+            str = str.slice(0, -1) + "}"
+			return str;
         }
         
     }
